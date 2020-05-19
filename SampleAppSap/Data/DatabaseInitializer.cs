4fc5a6cbef6950b88acd1cece2705041ecd8dbc6
Windesson Almeida <windesson.almeida@boeing.com>
2020-05-19 14:50:09 -0400

    initial commit

---
+++ SampleAppSap/Data/DatabaseInitializer.cs
@@ -0,0 +1,24 @@
+﻿using SampleAppSap.Models;
+using System;
+using System.Data.Entity;
+
+namespace SampleAppSap.Data
+{
+    internal class DatabaseInitializer : DropCreateDatabaseIfModelChanges<SampleAppSapContext>
+    {
+        protected override void Seed(SampleAppSapContext context)
+        {
+
+            context.Movies.Add( new Movie()
+            {
+                ID = 0,
+                Title = "ActionTitle",
+                Year = 2020,
+                Genre = "Action",
+                Rating = "PG-13" }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SampleAppSap/Models/SampleAppSapContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using SampleAppSap.Data;
+
+namespace SampleAppSap.Models
+{
+    public class SampleAppSapContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+
+        public SampleAppSapContext() : base("name=SampleAppSapContext")
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
+
+        public DbSet<SampleAppSap.Models.Movie> Movies { get; set; }
+    }
+}
